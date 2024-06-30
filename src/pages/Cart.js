f8b70614ec04eab0e0f1995ac1103e81b9e0393f
biggB01 <matprpic@gmail.com>
2024-06-30 22:59:06 +0200

    finished cart + fonts

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaTrashCan } from 'react-icons/fa6'; // Correct import
 import { CartContext } from '../context/CartContext';
 import './Cart.css';
 
@@ -11,25 +12,38 @@ const Cart = () => {
         navigate('/payment');
     };
 
+    const calculateTotal = () => {
+        return cart.reduce((total, game) => total + game.price, 0).toFixed(2);
+    };
+
     return (
-        <div>
-            <h2>Your Cart</h2>
-            <div className="cart-list">
-                {cart.map((game, index) => (
-                    <div key={index} className="cart-item">
-                        <img src={game.image_url} alt={`${game.name} cover`} />
-                        <div className="cart-item-details">
-                            <h3>{game.name}</h3>
-                            <p>{game.platform}</p>
-                            <p>${game.price}</p>
-                            <button onClick={() => removeFromCart(game.id)} className="remove-from-cart-button">Remove</button>
+        <div className="cart-container">
+            <div className="cart">
+                <h2>Cart</h2>
+                <div className="cart-list">
+                    {cart.map((game, index) => (
+                        <div key={index} className="cart-item">
+                            <img src={game.image_url} alt={`${game.name} cover`} className="cart-item-image" />
+                            <div className="cart-item-details">
+                                <h3>{game.name}</h3>
+                                <p>{game.platform}</p>
+                                <p>{game.price}€</p>
+                            </div>
+                            <button onClick={() => removeFromCart(game.id)} className="remove-from-cart-button">
+                                <FaTrashCan />
+                            </button>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-            {cart.length > 0 && (
-                <button onClick={handleCheckout} className="checkout-button">Proceed to Checkout</button>
-            )}
+            <div className="summary">
+                <h2>Summary</h2>
+                <p>Official price: 39.99€</p>
+                <p>Discount: 25.36€</p>
+                <p>Subtotal: {calculateTotal()}€</p>
+                <button onClick={handleCheckout} className="checkout-button">Go to payment</button>
+                <p><a href="/shop" className="continue-shopping">Continue shopping</a></p>
+            </div>
         </div>
     );
 };
