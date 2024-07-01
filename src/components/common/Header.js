bcc0836f00a16d9acd29cf8f0dac3f1f31070ba0
AntonPole <anton.poleksic@student.uniri.hr>
2024-07-01 22:57:27 +0200

    Search bar fix

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -27,50 +27,50 @@ const Header = ({ searchQuery, setSearchQuery }) => {
     };
 
     return (
-        <nav className="header">
-            <div className="header-left">
-                <button className="menu-button" onClick={toggleMenu}>
-                    <FontAwesomeIcon icon={faBars} />
-                </button>
-                <Link to="/" className="header-logo-link">
-                    <div className="header-logo">
-                        <img src={logo} alt="site logo" />
-                    </div>
-                    <span className="header-title">Kupi Key</span>
-                </Link>
-            </div>
-            <ul className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
-                <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('pc-section'); }}>PC</Link></li>
-                <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('playstation-section'); }}>PLAYSTATION</Link></li>
-                <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('xbox-section'); }}>XBOX</Link></li>
-                <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('nintendo-section'); }}>NINTENDO</Link></li>
-                <li className="profile-link-mobile"><Link to="/profile">PROFILE</Link></li> {/* Profile link for mobile view */}
-            </ul>
-            <div className="header-right">
-                <button className="search-button" onClick={toggleSearch}>
-                    <FontAwesomeIcon icon={faSearch} />
-                </button>
-                <Link to="/cart" className="icon-link">
-                    <FontAwesomeIcon icon={faShoppingCart} className="icon" />
-                    {cart.length > 0 && <span className="cart-count">{cart.length}</span>}
-                </Link>
-                <Link to="/profile" className="icon-link profile-icon">
-                    <FontAwesomeIcon icon={faUser} className="icon" />
-                </Link>
-            </div>
-            {isSearchOpen && (
-                <div className="search-bar-overlay">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="search-bar"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
+        <>
+            <nav className="header">
+                <div className="header-left">
+                    <button className="menu-button" onClick={toggleMenu}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    <Link to="/" className="header-logo-link">
+                        <div className="header-logo">
+                            <img src={logo} alt="site logo" />
+                        </div>
+                        <span className="header-title">Kupi Key</span>
+                    </Link>
                 </div>
-            )}
-            {isMenuOpen && <div className="overlay" onClick={toggleMenu}></div>}
-        </nav>
+                <ul className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
+                    <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('pc-section'); }}>PC</Link></li>
+                    <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('playstation-section'); }}>PLAYSTATION</Link></li>
+                    <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('xbox-section'); }}>XBOX</Link></li>
+                    <li><Link to="/" onClick={(e) => { e.preventDefault(); scrollToSection('nintendo-section'); }}>NINTENDO</Link></li>
+                    <li className="profile-link-mobile"><Link to="/profile">PROFILE</Link></li> {/* Profile link for mobile view */}
+                </ul>
+                <div className="header-right">
+                    <button className="search-button" onClick={toggleSearch}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                    <Link to="/cart" className="icon-link">
+                        <FontAwesomeIcon icon={faShoppingCart} className="icon" />
+                        {cart.length > 0 && <span className="cart-count">{cart.length}</span>}
+                    </Link>
+                    <Link to="/profile" className="icon-link profile-icon">
+                        <FontAwesomeIcon icon={faUser} className="icon" />
+                    </Link>
+                </div>
+                {isMenuOpen && <div className="overlay" onClick={toggleMenu}></div>}
+            </nav>
+            <div className={`search-bar-container ${isSearchOpen ? 'active' : ''}`}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="search-bar"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+            </div>
+        </>
     );
 };
 
